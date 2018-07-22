2c99e35562aef361bc618dc6a2e07c091268578d
katiejg <jng.katie@gmail.com>
2018-07-22 22:50:58 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -5,11 +5,11 @@ def initialize(activity = "collective punishment")
   @activity=activity
 end
 def students
-  students = [ ]
+  @@students = [ ]
+end
 def add_student(student)
   students
   self.students << student.to_s
   return self.students
-end
 end
 end
